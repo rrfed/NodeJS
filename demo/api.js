80a99177142e9d68802f77f02d7584d2a02f7d52
stepday <stepday@users.noreply.github.com>
2019-03-13 17:59:12 +0800

    express构建mock服务

---
+++ demo/api.js
@@ -0,0 +1,41 @@
+//引入express插件
+var express = require('express');
+var app = express();
+
+
+//body解析
+var bodyParser = require('body-parser');
+
+app.use(bodyParser.urlencoded({
+	extended: false
+}));
+
+
+app.use(express.static("static"));
+
+app.get('/',(req,res) =>{
+	res.send('Hello NodeJs');
+});
+
+app.get("/my",(req,res) =>{
+	//
+	console.log(req.query.name);
+	
+	res.json({
+		code:1,
+		msg:"ok",
+		info:{
+			name:"stepday"
+		}
+	});
+});
+
+//用户登录
+app.post("/login",(req,res) =>{
+	console.log(req.body);
+});
+
+//监听服务
+app.listen(8081,function(){
+	console.log("服务已经开启");
+});
